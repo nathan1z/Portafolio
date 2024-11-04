22b1d6a0695f4bcc96f9e4e527c9a30cb4d41767
jonathan.zapata <84453014+JonathanZapata1@users.noreply.github.com>
2024-11-04 12:46:36 -0500

    Implementación de text overlay y botón de visita en imágenes

--- src/Components/MyWork/MyWork.jsx
+++ src/Components/MyWork/MyWork.jsx
@@ -1,25 +1,35 @@
-import './MyWork.css'
-import mywork_data from '../../assets/mywork_data'
-import arrow_icon from '../../assets/arrow_icon.svg'
+import "./MyWork.css";
+import mywork_data from "../../assets/mywork_data";
+import arrow_icon from "../../assets/arrow_icon.svg";
 const MyWork = () => {
   return (
-    <div id="work" className='mywork'>
-     <div className="mywork-title">
+    <div id="work" className="mywork">
+      <div className="mywork-title">
         <h1>My Latest work</h1>
-        </div> 
-        <div className="mywork-container">
-            {mywork_data.map((work,index)=>{
-                return <img key={index} src={work.w_img} alt=''/>
-            })}
-        </div> 
-        <a href="https://github.com/nathan1z?tab=repositories" target="_blank" rel="noopener noreferrer" className='git'>
+      </div>
+      <div className="mywork-container">
+        {mywork_data.map((work, index) => {
+          return (
+            <div className="image-container" key={index}>
+              <img src={work.w_img} alt="" />
+              <span className="hover-text">{work.description}</span>
+            </div>
+          );
+        })}
+      </div>
+      <a
+        href="https://github.com/nathan1z?tab=repositories"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="git"
+      >
         <div className="mywork-showmore">
           <p>Show More</p>
           <img src={arrow_icon} alt="" />
-          </div>
-          </a> 
+        </div>
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default MyWork
+export default MyWork;
--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -9,32 +9,38 @@ const mywork_data = [
     {
         w_no:1,
         w_name:"Web design",
-        w_img:project1_img
+        w_img:project1_img,
+        description:"An integrated solution for managing orders, payments, and reservations to streamline restaurant operations and enhance customer experience, built with JavaScript, React.js, and Bootstrap using a modular component structure.",
     },
     {
         w_no:2,
         w_name:"Web design",
-        w_img:project2_img
+        w_img:project2_img,
+        description:"A digital solution for marketing bonsais that provides an intuitive and personalized shopping experience for gardening enthusiasts, utilizing JavaScript, React.js with JSX for component structure, Bootstrap and FontAwesome for styling and icons, and a modular project organization with a MongoDB database."
     },
     {
         w_no:3,
         w_name:"Web design",
-        w_img:project3_img
+        w_img:project3_img,
+        description:"A machine learning model designed to effectively detect ransomware attacks by automating the analysis of executable files (.exe and .dll), enabling early detection of ransomware in production environments, utilizing Python technology."
     },
     {
         w_no:4,
         w_name:"Web design",
-        w_img:project4_img
+        w_img:project4_img,
+        description:"This platform is a primary solution for acquiring blogs and facilitating communication among a small group of people, utilizing JavaScript, React, and a MongoDB database."
     },
     {
         w_no:5,
         w_name:"Web design",
-        w_img:project5_img
+        w_img:project5_img,
+        description:"The acquisition of this platform for hardware billing aims to validate purchases and enhance the effectiveness of tracking purchase records, utilizing JavaScript, Django, and Python technologies."
     },
     {
         w_no:6,
         w_name:"Web design",
-        w_img:project6_img
+        w_img:project6_img,
+        description:"GitHub repository showcasing evidence of all the technologies used."
     },
 ]
  
